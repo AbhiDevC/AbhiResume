d17c51d24f6b2e60ea0570092aa18b1874e7b4dc
AbhiDevC <129884655+AbhiDevC@users.noreply.github.com>
2024-08-16 17:32:44 -0400

    added routing and styling  change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,11 @@ import { NgModule } from '@angular/core';
 
 
 export const routes: Routes = [
-   
-        { path: '', component: HomeComponent },
+        { path: '', redirectTo: '/home', pathMatch: 'full' },
+        { path: 'home', component: HomeComponent },
         { path: 'skills', component: SkillsComponent },
+
+        { path: '**', redirectTo: '/home' }
       
 ];
  
