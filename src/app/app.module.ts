30c295512e54979b1f0b2b7bf6203955cbb2410e
Abhinandan Sharma <abhinandansharma@Abhinandans-MacBook-Pro.local>
2025-09-24 22:28:20 -0400

    added contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { CarouselModule } from '@coreui/angular';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { CarouselModule } from '@coreui/angular';
     EducationComponent,
     SkillsComponent,
     ExpereienceComponent,
+    ContactPageComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,17 +5,15 @@ import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { EducationComponent } from './education/education.component';
 import { ExpereienceComponent } from './expereience/expereience.component';
-
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 export const routes: Routes = [
-        { path: '', redirectTo: '/home', pathMatch: 'full' },
-        { path: 'home', component: HomeComponent },
-        { path: 'skills', component: SkillsComponent },
-        {path:'education', component:EducationComponent},
-        {path:'experience', component:ExpereienceComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'experience', component: ExpereienceComponent },
+  { path: 'contact', component: ContactPageComponent },
 
-
-        { path: '**', redirectTo: '/home' }
-      
+  { path: '**', redirectTo: '/home' },
 ];
- 
